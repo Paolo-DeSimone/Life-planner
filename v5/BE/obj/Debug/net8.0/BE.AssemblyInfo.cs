c81a3e7c13dff5d44f90113db74bb4e0c70ef044
Paolo De Simone - Kappas.it <paolods22299@gmail.com>
2025-02-17 11:13:13 +0100

    FE created. FE and BE both working simultaneously
    from v5/v5Configs wrtite in console npm run start-all to launch both FE and BE

--- v5/BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ v5/BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291812313dad87afd3f889fe99f3d908a45264aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e1fb2034bc5f70104403cecc9eb80304819a367")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
